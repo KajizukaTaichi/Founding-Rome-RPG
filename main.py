ce2befeac7c2b831e40be44f5c4bb4be8336a7dc
梶塚太智 <122075081+KajizukaTaichi@users.noreply.github.com>
2023-12-10 19:17:44 +0900

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ class Rome:
     def opening(self):
         msg = ["双子の兄弟、ロムルスとレムスは、神々の子として古代のアルバ・ロンガで誕生した",
             "彼らは運命に導かれ羊飼いに育てられたが、彼らの運命は、遥かなるローマの創設にあった。",
-            "ロムルスとレムスの成長と絆が続く中、兄弟は分かれることを余儀なくされたるという悲劇的な運命び転換を迎える。",
+            "ロムルスとレムスの成長と絆が続く中、兄弟は分かれることを余儀なくされたるという悲劇的な運命の転換を迎える。",
             "しかし、ロムルス王の冒険は力と栄光の都市を求め古代の大地にローマの物語が幕を開けるのだった。\n" ]
         
         for i in msg:
@@ -21,9 +21,9 @@ class Rome:
 
     def policy(self):
         input()
-        print("レギオー:", self.army)
-        print("市民の士気:", self.morale)
-        print("ローマ都市:レベル", self.city)
+        print("レギオー: 軍事レベル", self.army)
+        print("市民の士気: レベル", self.morale)
+        print("ローマ: 都市レベル", self.city)
         print("経済力:", self.economic, "デナリウス")
         select = input("元老院　{ A:都市建設する？　B:戦争する？　C:民会で演説する？")
         if "A" in select or "a" in select:
@@ -53,7 +53,7 @@ class Rome:
     def speech(self, cost):
         self.morale += cost; self.economic -= cost
         input("市民 { いいぞ！ロムルス王")
-        print(f"市民の士気が{cost}アップ!")
+        print(f"市民の士気がレベル{cost}アップ!")
 
     def battle_war(self, cost: int):
         print("戦争開始！ワーワー")
@@ -61,16 +61,16 @@ class Rome:
         self.army += cost; self.economic -= cost
         
         if ((self.army + self.morale) / 2)  > enemy:
-            print("敵は弱いぞ:軍事力が",enemy,"しかない")
+            print("敵は弱いぞ:軍事レベルが",enemy,"しかない")
             input("絶対勝てる、勇気を持って行くぞー！")
-            print("おー！")
+            print("市民 { おー！")
 
             print(f"勝利！賠償金{(reward := int(enemy * 2))}デナリウス獲得") 
             self.economic += reward
-            print(f"市民の士気が{reward}アップ！")
+            print(f"市民の士気がレベル{reward}アップ！")
             self.morale += reward
         else:
-            print("敵は強いぞ:軍事力が",enemy,"もある")
+            print("敵は強いぞ:軍事レベルが",enemy,"もある")
 
             select = input("元老院 { 勝ち目がないな、和平しとく？ (Yes/No)")
             if select == "No":
@@ -78,14 +78,14 @@ class Rome:
 
                 print(f"敗北・・・。賠償金{(damage := int(enemy * 2))}デナリウス払う") 
                 self.economic -= damage
-                print(f"市民の士気が{damage}ダウン")
+                print(f"市民の士気がレベル{damage}ダウン")
                 self.morale -= damage
             else:
                 print("元老院は和平の決議をし、ローマは敵と和平を結んだ！")
 
     def develop_city(self, cost: int):
         print(f"街を{cost}デナリウス分立派にした！")
-        self.city += cost; self.economic -= cost
+        self.city += (cost + self.morale); self.economic -= cost
         print(f"報酬{(reward := int(self.city / 6))}デナリウス獲得！")
         self.economic += reward
         self.is_clear()
@@ -98,7 +98,7 @@ class Rome:
             input("ゲームクリア！")
             input("ユピテルが風を遣わしてロムルス王の伝説の生涯は終わりましたとさ")
             input("めでたしめでたし")
-            exit(0) 
+            exit(0)
 
 
 if __name__ == "__main__":
